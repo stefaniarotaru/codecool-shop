96907d9a6f99c03b85c3cecf53ca49d14525e350
stefaniarotaru <rotaru.g.stefania@gmail.com>
2021-04-26 21:29:53 +0300

    added Address, Order, User, LineItem

---
+++ src/main/java/com/codecool/shop/model/User.java
@@ -0,0 +1,59 @@
+package com.codecool.shop.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %1$d," +
+                        "name: %2$s," +
+                        "email: %3$s" +
+                        "phone: %4$s",
+                this.id,
+                this.name,
+                this.email,
+                this.phone);
+
+    }
+}
